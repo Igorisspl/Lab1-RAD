e214a5651d06951d545feb1b0efbfac8cff13319
Igorisspl <S00238949@atu.ie>
2024-09-30 12:09:07 +0100

    Add project files.

---
+++ Lab 1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class Supplier
+    {
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
